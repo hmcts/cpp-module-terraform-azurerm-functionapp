e1d17e967ea977f8528c911fc5b6bcb3d1628400
David Amanshia <dave@piranha.sh>
2022-10-27 18:04:22 +0100

    count fix

--- eventgrid.tf
+++ eventgrid.tf
@@ -9,11 +9,11 @@ resource "azurerm_eventgrid_topic" "function_app_eventgrid_topic" {
 
 # Standard Topic Subscription
 resource "azurerm_eventgrid_event_subscription" "function_app_eventgrid_topic" {
-  depends_on = [azurerm_eventgrid_topic.function_app_eventgrid_topic]
+  depends_on = [azurerm_eventgrid_topic.function_app_eventgrid_topic,azurerm_windows_function_app.windows_function,azurerm_linux_function_app.linux_function]
   name       = "EGS-${var.environment}-${var.namespace}-${var.application}"
   scope      = data.azurerm_resource_group.main.id
   azure_function_endpoint {
-    function_id = length(azurerm_windows_function_app.windows_function) == 1 ? azurerm_windows_function_app.windows_function.id : azurerm_linux_function_app.linux_function.id
+    function_id = length(azurerm_windows_function_app.windows_function) == 1 ? azurerm_windows_function_app.windows_function[0].id : azurerm_linux_function_app.linux_function[0].id
   }
   for_each = var.eventgrid_topic_subscriptions
 }
@@ -30,11 +30,11 @@ resource "azurerm_eventgrid_system_topic" "function_app_eventgrid_system_topic" 
 
 # System Topic Subscription
 resource "azurerm_eventgrid_event_subscription" "function_app_eventgrid_system_topic" {
-  depends_on = [azurerm_eventgrid_system_topic.function_app_eventgrid_system_topic]
+  depends_on = [azurerm_eventgrid_system_topic.function_app_eventgrid_system_topic,azurerm_windows_function_app.windows_function,azurerm_linux_function_app.linux_function]
   name       = "EGSTS-${var.environment}-${var.namespace}-${var.application}"
   scope      = data.azurerm_resource_group.main.id
   azure_function_endpoint {
-    function_id = length(azurerm_windows_function_app.windows_function) == 1 ? azurerm_windows_function_app.windows_function.id : azurerm_linux_function_app.linux_function.id
+    function_id = length(azurerm_windows_function_app.windows_function) == 1 ? azurerm_windows_function_app.windows_function[0].id : azurerm_linux_function_app.linux_function[0].id
   }
   for_each = var.eventgrid_system_topic_subscriptions
 }
