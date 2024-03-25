48fb631f1f88304051dcdec58c6d3a481c23bda4
varunvijayagopal <varun.vijayagopal@hmcts.net>
2024-03-25 16:16:44 +0000

    cert support

--- main.tf
+++ main.tf
@@ -65,12 +65,12 @@ resource "azurerm_linux_function_app" "linux_function" {
   dynamic "storage_account" {
     for_each = var.storage_account_mount == {} ? [] : [var.storage_account_mount]
     content {
-      access_key   = lookup(storage_account_mount.value, "access_key", null)
-      account_name = lookup(storage_account_mount.value, "account_name", null)
-      name         = lookup(storage_account_mount.value, "name", "certs")
-      share_name   = lookup(storage_account_mount.value, "share_name", "certs")
-      type         = lookup(storage_account_mount.value, "type", "AzureFiles")
-      mount_path   = lookup(storage_account_mount.value, "mount_path", "/certs")
+      access_key   = lookup(storage_account.value, "access_key", null)
+      account_name = lookup(storage_account.value, "account_name", null)
+      name         = lookup(storage_account.value, "name", "certs")
+      share_name   = lookup(storage_account.value, "share_name", "certs")
+      type         = lookup(storage_account.value, "type", "AzureFiles")
+      mount_path   = lookup(storage_account.value, "mount_path", "/certs")
     }
   }
 
