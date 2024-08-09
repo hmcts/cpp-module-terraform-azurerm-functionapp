835c662ca6719eafc327ecaf1a3c47813f9926ef
swiftnickmoj <nicholas.holbrook1@hmcts.net>
2024-08-09 12:42:34 +0100

    Added dns_resource_group_name" to terratest

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -58,7 +58,7 @@ module "functionapp_terratest" {
   create_subnet                = true
   subnet_cidr                  = ["10.0.1.0/24"]
   dns_resource_group_name      = var.dns_resource_group_name
-  
+
   depends_on = [
     azurerm_virtual_network.test
   ]
