25ff27ef3d030890f11f0bfaf663e09a062a658a
jyothi.balla <jyothi.balla@hmcts.net>
2025-11-24 15:43:50 +0000

    Enable autoscale rule setting for app service plan

--- main.tf
+++ main.tf
@@ -274,12 +274,14 @@ resource "azurerm_windows_function_app" "windows_function" {
   builtin_logging_enabled       = var.builtin_logging_enabled
   virtual_network_subnet_id     = var.create_subnet && length(var.subnet_cidr) != 0 ? azurerm_subnet.main[0].id : var.subnet_id
   public_network_access_enabled = var.public_network_access_override
-  app_settings = {
+  app_settings = (
+      var.asp_os_type == "Windows" && var.storage_account_is_public_enable_map ? {
         WEBSITE_VNET_ROUTE_ALL                   = "1"
         WEBSITE_CONTENTAZUREFILECONNECTIONSTRING = var.storage_account_connection_string
         WEBSITE_CONTENTSHARE                     = var.storage_content_share
         WEBSITE_CONTENTOVERVNET                  = "1"
-  }
+      } : {}
+    )
   dynamic "identity" {
     for_each = var.identity == {} ? [] : [var.identity]
     content {
